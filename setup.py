c2b1f6b08376dc84591635918fb6a381fa297da1
Akshay Kamath <130640974+k4m4th@users.noreply.github.com>
2025-08-17 14:20:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Akshay Kamath",
     url="https://github.com/k4m4th/tensorweave",   # optional
     license="MIT",                                 # or what you use
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     packages=find_packages(exclude=("tests", "docs")),
     include_package_data=True,
     install_requires=[
